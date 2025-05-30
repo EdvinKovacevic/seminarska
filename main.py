c7563f4e5d4c81c5c447485093d6e667de4412c7
EdvinKovacevic <ek21879@student.uni-lj.si>
2025-05-30 19:45:11 +0200

    Added .gitignore and started main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+#https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&pid=2000001460000&rp=99999
+#https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&pid=2000001456000&rp=99999
+#https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&pid=2000001451000&rp=99999
+
+
+
+#https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=4&pid=2000001461000&mode=1&stype=1&othercon=2&releaseYStart=1999&releaseMStart=02&releaseDStart=04&sort=1&rp=99999&page=1
